0e5a7bf54bbb8fc26b9352e157af27416e122977
AmrKahla6 <amrkahla6@gmail.com>
2021-04-18 10:37:45 +0200

    Language

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(
+    ['prefix' => LaravelLocalization::setLocale(),'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+        Route::namespace('API')->group(function(){
+
+        });
+    });
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
